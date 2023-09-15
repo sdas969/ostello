9fd6707b6417ab0ba9350f09ba4733a6fe58e18a
Smarajit Das <sdas9696969@gmail.com>
2023-09-15 23:23:59 +0530

    Update

--- lib/universal/custom_header_text_field.dart
+++ lib/universal/custom_header_text_field.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:ostello/universal/text_field_suffix_buttons.dart';
 
 class CustomHeaderTextField extends StatelessWidget {
-  const CustomHeaderTextField({super.key, this.hint = ''});
+  const CustomHeaderTextField({super.key, this.hint = '', this.onTap});
   final String hint;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) => TextField(
+      onTap: onTap,
       decoration: InputDecoration(
           hintText: hint,
           suffixIcon: const TextFieldSuffixButtons(),
--- lib/views/home_screen/home_screen_header_content.dart
+++ lib/views/home_screen/home_screen_header_content.dart
@@ -2,18 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:ostello/universal/custom_header_text_field.dart';
 import 'package:ostello/views/home_screen/category_list_widget.dart';
 import 'package:ostello/views/search_screen/location_profile_widget.dart';
+import 'package:ostello/views/search_screen/search_screen.dart';
 
 class HomeScreenHeaderContent extends StatelessWidget {
   const HomeScreenHeaderContent({super.key});
 
   @override
-  Widget build(BuildContext context) => const Column(
+  Widget build(BuildContext context) => Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            LocationProfileWidget(),
-            SizedBox(height: 18),
-            CustomHeaderTextField(hint: "Search for coaching, exams"),
-            CategoryListWidget()
+            const LocationProfileWidget(),
+            const SizedBox(height: 18),
+            CustomHeaderTextField(
+              hint: "Search for coaching, exams",
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SearchScreen())),
+            ),
+            const CategoryListWidget()
           ]);
 }
